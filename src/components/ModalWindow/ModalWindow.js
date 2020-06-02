4d85cb0199841e9007a320959403b37d6c50732d
v-kiry <vkshazs@gmail.com>
2020-06-02 13:56:15 +0600

    Hotfix modal window

--- src/components/ModalWindow/ModalWindow.js
+++ src/components/ModalWindow/ModalWindow.js
@@ -7,7 +7,7 @@ import styles from './ModalWindow.style';
 const ModalWindow = ({onClick}) => {
   return (
     <div css={styles.containerModal} onClick={onClick}>
-      <div css={styles.windowModal}>
+      <div css={styles.windowModal} onClick={(event) => event.stopPropagation()}>
         <button css={styles.buttonModal} onClick={onClick}></button>
         <span>Information about element</span>
       </div>
